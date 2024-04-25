378cb93917342de5994a3eb3f4a9c316d6fb7cd9
Encephala <jonathan_rietveld@hotmail.com>
2024-04-25 13:36:48 +0200

    Implement printing to console
    Also more logical parameter name

--- src/evaluate/builtins.rs
+++ src/evaluate/builtins.rs
@@ -10,6 +10,7 @@ pub enum BuiltinFunction {
     First,
     Rest,
     Push,
+    Puts,
 }
 
 impl BuiltinFunction {
@@ -21,6 +22,7 @@ impl BuiltinFunction {
             First => first(arguments),
             Rest => rest(arguments),
             Push => push(arguments),
+            Puts => puts(arguments),
         }
     }
 }
@@ -34,6 +36,7 @@ impl std::fmt::Display for BuiltinFunction {
             First => f.write_str("first"),
             Rest => f.write_str("rest"),
             Push => f.write_str("push"),
+            Puts => f.write_str("puts"),
         }
     }
 }
@@ -49,62 +52,62 @@ impl ExecutionEnvironment {
     }
 }
 
-fn len(parameters: Vec<Object>) -> Result<Object, String> {
-    if parameters.len() != 1 {
-        return Err(format!("Called len with invalid number of parameters {}", parameters.len()));
+fn len(arguments: Vec<Object>) -> Result<Object, String> {
+    if arguments.len() != 1 {
+        return Err(format!("Called len with invalid number of arguments {}", arguments.len()));
     }
 
-    match parameters.first().unwrap() {
+    match arguments.first().unwrap() {
         Object::Str(value) => {
             Ok(Object::Int(value.len() as isize))
         }
         Object::Array(value) => {
             Ok(Object::Int(value.len() as isize))
         },
-        other => Err(format!("Parameter {other:?} is in valid in len"))
+        other => Err(format!("Argument {other:?} is in valid in len"))
     }
 }
 
-fn first(parameters: Vec<Object>) -> Result<Object, String> {
-    if parameters.len() != 1 {
-        return Err(format!("Called first with invalid number of parameters {}", parameters.len()));
+fn first(arguments: Vec<Object>) -> Result<Object, String> {
+    if arguments.len() != 1 {
+        return Err(format!("Called first with invalid number of arguments {}", arguments.len()));
     }
 
-    match parameters.first().unwrap() {
+    match arguments.first().unwrap() {
         Object::Array(value) => {
             if value.is_empty() {
-                Err(format!("Parameter {value:?} is empty in first"))
+                Err(format!("Argument {value:?} is empty in first"))
             } else {
                 Ok(value.first().unwrap().clone())
             }
         },
-        other => Err(format!("Parameter {other:?} is invalid in first")),
+        other => Err(format!("Argument {other:?} is invalid in first")),
     }
 }
 
-fn rest(parameters: Vec<Object>) -> Result<Object, String> {
-    if parameters.len() != 1 {
-        return Err(format!("Called rest with invalid number of parameters {}", parameters.len()));
+fn rest(arguments: Vec<Object>) -> Result<Object, String> {
+    if arguments.len() != 1 {
+        return Err(format!("Called rest with invalid number of arguments {}", arguments.len()));
     }
 
-    match parameters.first().unwrap() {
+    match arguments.first().unwrap() {
         Object::Array(value) => {
             if value.is_empty() {
-                Err(format!("Parameter {value:?} is empty in rest"))
+                Err(format!("Argument {value:?} is empty in rest"))
             } else {
                 Ok(Object::Array(value.into_iter().skip(1).cloned().collect()))
             }
         },
-        other => Err(format!("Parameter {other:?} is invalid in first")),
+        other => Err(format!("Argument {other:?} is invalid in first")),
     }
 }
 
-fn push(parameters: Vec<Object>) -> Result<Object, String> {
-    if parameters.len() != 2 {
-        return Err(format!("Called push with invalid number of parameters {}", parameters.len()));
+fn push(arguments: Vec<Object>) -> Result<Object, String> {
+    if arguments.len() != 2 {
+        return Err(format!("Called push with invalid number of arguments {}", arguments.len()));
     }
 
-    let mut iter = parameters.iter().cloned();
+    let mut iter = arguments.iter().cloned();
 
     let array = iter.next().unwrap();
     let new = iter.next().unwrap();
@@ -114,6 +117,15 @@ fn push(parameters: Vec<Object>) -> Result<Object, String> {
             array.push(new);
             Ok(Object::Array(array))
         },
-        other => Err(format!("Parameter {other:?} is invalid in push")),
+        other => Err(format!("Argument {other:?} is invalid in push")),
     }
+}
+
+fn puts(arguments: Vec<Object>) -> Result<Object, String> {
+    println!("{}", arguments.iter()
+        .map(|argument| argument.to_string())
+        .collect::<Vec<_>>()
+        .join(" "));
+
+    return Ok(Object::None);
 }
